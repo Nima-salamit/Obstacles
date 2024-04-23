151b9348cf9721b563b9f729ce10e83b9321a71d
Nima Sl <110297816+Nima-salamit@users.noreply.github.com>
2024-04-23 11:53:42 +0330

    GetAxis commit

---
+++ Obstacles/Assets/Scripts/Mover.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    // These variables change our movement speed
+    [SerializeField] float yValue = 0.001f;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xValue = Input.GetAxis("Horizontal");
+        float zValue = Input.GetAxis("Vertical");
+        transform.Translate(xValue,yValue,zValue); 
+    }
+}
